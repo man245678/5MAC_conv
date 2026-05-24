3932b4c235221e1c171c0043d5f678edff40ddc4
hyeonjae <hyeonjae@local>
2026-05-24 18:01:32 +0900

    Use scalar accumulator in 5MAC convolver

--- Convolver.v
+++ Convolver.v
@@ -35,22 +35,20 @@ module Convolver
     localparam ISSUE_IMAGE   = 4'd4;
     localparam WAIT_IMAGE    = 4'd5;
     localparam STORE_IMAGE   = 4'd6;
-    localparam CLEAR_PSUM    = 4'd7;
+    localparam CLEAR_ACC     = 4'd7;
     localparam COMPUTE       = 4'd8;
     localparam WRITE_FEATURE = 4'd9;
     localparam DONE          = 4'd10;
 
     reg [3:0] cur_state, next_state;
-
     reg [6:0] cur_filter_idx, next_filter_idx;
     reg [6:0] cur_load_row, next_load_row;
     reg [8:0] cur_load_idx, next_load_idx;
+    reg [4:0] cur_feature_x, next_feature_x;
     reg [4:0] cur_feature_y, next_feature_y;
-    reg [4:0] cur_compute_x, next_compute_x;
     reg [2:0] cur_kernel_row, next_kernel_row;
     reg [1:0] cur_channel, next_channel;
-    reg [4:0] cur_clear_idx, next_clear_idx;
-    reg [4:0] cur_write_idx, next_write_idx;
+    reg signed [2*BITWIDTH-1:0] cur_acc, next_acc;
 
     reg signed [BITWIDTH-1:0] row_buf0 [0:3*IMAGE_WIDTH-1];
     reg signed [BITWIDTH-1:0] row_buf1 [0:3*IMAGE_WIDTH-1];
@@ -58,7 +56,6 @@ module Convolver
     reg signed [BITWIDTH-1:0] row_buf3 [0:3*IMAGE_WIDTH-1];
     reg signed [BITWIDTH-1:0] row_buf4 [0:3*IMAGE_WIDTH-1];
     reg signed [BITWIDTH-1:0] filter_buf [0:3*FILTER_WIDTH*FILTER_WIDTH-1];
-    reg signed [2*BITWIDTH-1:0] psum [0:FEATURE_WIDTH-1];
 
     wire [1:0] load_channel = (cur_load_idx < IMAGE_WIDTH) ? 0 :
                               ((cur_load_idx < 2*IMAGE_WIDTH) ? 1 : 2);
@@ -70,7 +67,7 @@ module Convolver
 
     wire [6:0] active_abs_row = cur_feature_y * 3 + cur_kernel_row;
     wire [2:0] active_slot = active_abs_row % 5;
-    wire [6:0] base_col = cur_compute_x * 3;
+    wire [6:0] base_col = cur_feature_x * 3;
     wire [8:0] active_base_col = cur_channel * IMAGE_WIDTH + base_col;
 
     wire [8:0] idx_col0 = active_base_col + 0;
@@ -87,11 +84,11 @@ module Convolver
 
     wire [6:0] filter_base = cur_channel * FILTER_WIDTH * FILTER_WIDTH +
                              cur_kernel_row * FILTER_WIDTH;
-    wire signed [BITWIDTH-1:0] filter1  = filter_buf[filter_base + 0];
-    wire signed [BITWIDTH-1:0] filter2  = filter_buf[filter_base + 1];
-    wire signed [BITWIDTH-1:0] filter3  = filter_buf[filter_base + 2];
-    wire signed [BITWIDTH-1:0] filter4  = filter_buf[filter_base + 3];
-    wire signed [BITWIDTH-1:0] filter5  = filter_buf[filter_base + 4];
+    wire signed [BITWIDTH-1:0] filter1 = filter_buf[filter_base + 0];
+    wire signed [BITWIDTH-1:0] filter2 = filter_buf[filter_base + 1];
+    wire signed [BITWIDTH-1:0] filter3 = filter_buf[filter_base + 2];
+    wire signed [BITWIDTH-1:0] filter4 = filter_buf[filter_base + 3];
+    wire signed [BITWIDTH-1:0] filter5 = filter_buf[filter_base + 4];
 
     wire signed [2*BITWIDTH-1:0] mac_result;
 
@@ -102,8 +99,8 @@ module Convolver
     assign IMAGE_RAM_ADDRESS = load_channel * IMAGE_WIDTH * IMAGE_WIDTH +
                                cur_load_row * IMAGE_WIDTH + load_col;
     assign FILTER_RAM_ADDRESS = cur_filter_idx;
-    assign FEATURE_RAM_ADDRESS = cur_write_idx + cur_feature_y * FEATURE_WIDTH;
-    assign FEATURE_RAM_DOUT = psum[cur_write_idx];
+    assign FEATURE_RAM_ADDRESS = cur_feature_x + cur_feature_y * FEATURE_WIDTH;
+    assign FEATURE_RAM_DOUT = cur_acc;
     assign eoc = (cur_state == DONE);
 
     MAC #(
@@ -131,24 +128,22 @@ module Convolver
             cur_filter_idx <= 0;
             cur_load_row <= 0;
             cur_load_idx <= 0;
+            cur_feature_x <= 0;
             cur_feature_y <= 0;
-            cur_compute_x <= 0;
             cur_kernel_row <= 0;
             cur_channel <= 0;
-            cur_clear_idx <= 0;
-            cur_write_idx <= 0;
+            cur_acc <= 0;
         end
         else begin
             cur_state <= next_state;
             cur_filter_idx <= next_filter_idx;
             cur_load_row <= next_load_row;
             cur_load_idx <= next_load_idx;
+            cur_feature_x <= next_feature_x;
             cur_feature_y <= next_feature_y;
-            cur_compute_x <= next_compute_x;
             cur_kernel_row <= next_kernel_row;
             cur_channel <= next_channel;
-            cur_clear_idx <= next_clear_idx;
-            cur_write_idx <= next_write_idx;
+            cur_acc <= next_acc;
         end
     end
 
@@ -169,28 +164,16 @@ module Convolver
         end
     end
 
-    always @ (posedge clk or negedge resetn) begin
-        if(!resetn) begin
-        end
-        else begin
-            if(cur_state == CLEAR_PSUM)
-                psum[cur_clear_idx] <= 0;
-            else if(cur_state == COMPUTE)
-                psum[cur_compute_x] <= psum[cur_compute_x] + mac_result;
-        end
-    end
-
     always @ (*) begin
         next_state = cur_state;
         next_filter_idx = cur_filter_idx;
         next_load_row = cur_load_row;
         next_load_idx = cur_load_idx;
+        next_feature_x = cur_feature_x;
         next_feature_y = cur_feature_y;
-        next_compute_x = cur_compute_x;
         next_kernel_row = cur_kernel_row;
         next_channel = cur_channel;
-        next_clear_idx = cur_clear_idx;
-        next_write_idx = cur_write_idx;
+        next_acc = cur_acc;
 
         case(cur_state)
             IDLE: begin
@@ -198,12 +181,11 @@ module Convolver
                 next_filter_idx = 0;
                 next_load_row = 0;
                 next_load_idx = 0;
+                next_feature_x = 0;
                 next_feature_y = 0;
-                next_compute_x = 0;
                 next_kernel_row = 0;
                 next_channel = 0;
-                next_clear_idx = 0;
-                next_write_idx = 0;
+                next_acc = 0;
             end
             ISSUE_FILTER: begin
                 next_state = WAIT_FILTER;
@@ -235,8 +217,7 @@ module Convolver
                     next_load_idx = 0;
                     if((cur_feature_y == 0 && cur_load_row == 4) ||
                        (cur_feature_y != 0 && cur_load_row == cur_feature_y*3 + 4)) begin
-                        next_state = CLEAR_PSUM;
-                        next_clear_idx = 0;
+                        next_state = CLEAR_ACC;
                     end
                     else begin
                         next_load_row = cur_load_row + 1;
@@ -248,18 +229,15 @@ module Convolver
                     next_state = ISSUE_IMAGE;
                 end
             end
-            CLEAR_PSUM: begin
-                if(cur_clear_idx == FEATURE_WIDTH-1) begin
-                    next_state = COMPUTE;
-                    next_compute_x = 0;
-                    next_kernel_row = 0;
-                    next_channel = 0;
-                end
-                else begin
-                    next_clear_idx = cur_clear_idx + 1;
-                end
+            CLEAR_ACC: begin
+                next_acc = 0;
+                next_kernel_row = 0;
+                next_channel = 0;
+                next_state = COMPUTE;
             end
             COMPUTE: begin
+                next_acc = cur_acc + mac_result;
+
                 if(cur_channel != 2) begin
                     next_channel = cur_channel + 1;
                 end
@@ -267,33 +245,27 @@ module Convolver
                     next_channel = 0;
                     next_kernel_row = cur_kernel_row + 1;
                 end
-                else if(cur_compute_x == FEATURE_WIDTH-1) begin
-                    next_channel = 0;
-                    next_kernel_row = 0;
-                    next_compute_x = 0;
-                    next_state = WRITE_FEATURE;
-                    next_write_idx = 0;
-                end
                 else begin
                     next_channel = 0;
                     next_kernel_row = 0;
-                    next_compute_x = cur_compute_x + 1;
+                    next_state = WRITE_FEATURE;
                 end
             end
             WRITE_FEATURE: begin
-                if(cur_write_idx == FEATURE_WIDTH-1) begin
-                    if(cur_feature_y == FEATURE_WIDTH-1) begin
-                        next_state = DONE;
-                    end
-                    else begin
-                        next_feature_y = cur_feature_y + 1;
-                        next_load_row = (cur_feature_y + 1) * 3 + 2;
-                        next_load_idx = 0;
-                        next_state = ISSUE_IMAGE;
-                    end
+                if((cur_feature_x == FEATURE_WIDTH-1) &&
+                   (cur_feature_y == FEATURE_WIDTH-1)) begin
+                    next_state = DONE;
+                end
+                else if(cur_feature_x == FEATURE_WIDTH-1) begin
+                    next_feature_x = 0;
+                    next_feature_y = cur_feature_y + 1;
+                    next_load_row = (cur_feature_y + 1) * 3 + 2;
+                    next_load_idx = 0;
+                    next_state = ISSUE_IMAGE;
                 end
                 else begin
-                    next_write_idx = cur_write_idx + 1;
+                    next_feature_x = cur_feature_x + 1;
+                    next_state = CLEAR_ACC;
                 end
             end
             DONE: begin
