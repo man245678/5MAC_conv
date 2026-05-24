cef1baab54ec2654c275f499b654012d1230907f
hyeonjae <hyeonjae@local>
2026-05-24 21:59:43 +0900

    Bank and pipeline 5MAC window reads

--- Convolver.v
+++ Convolver.v
@@ -1,40 +1,50 @@
 `define DECL_WIN(A0,A1,A2,A3,A4) \
-    reg signed [BITWIDTH-1:0] A0 [0:FEATURE_WIDTH-1]; \
-    reg signed [BITWIDTH-1:0] A1 [0:FEATURE_WIDTH-1]; \
-    reg signed [BITWIDTH-1:0] A2 [0:FEATURE_WIDTH-1]; \
-    reg signed [BITWIDTH-1:0] A3 [0:FEATURE_WIDTH-1]; \
-    reg signed [BITWIDTH-1:0] A4 [0:FEATURE_WIDTH-1]
+    reg signed [BITWIDTH-1:0] A0 [0:3][0:7]; \
+    reg signed [BITWIDTH-1:0] A1 [0:3][0:7]; \
+    reg signed [BITWIDTH-1:0] A2 [0:3][0:7]; \
+    reg signed [BITWIDTH-1:0] A3 [0:3][0:7]; \
+    reg signed [BITWIDTH-1:0] A4 [0:3][0:7]
 
-`define CAPTURE_SLOT(A00,A01,A02,A03,A04,A10,A11,A12,A13,A14,A20,A21,A22,A23,A24) begin \
-    slot_ch0_1 <= A00[cur_feature_x]; \
-    slot_ch0_2 <= A01[cur_feature_x]; \
-    slot_ch0_3 <= A02[cur_feature_x]; \
-    slot_ch0_4 <= A03[cur_feature_x]; \
-    slot_ch0_5 <= A04[cur_feature_x]; \
-    slot_ch1_1 <= A10[cur_feature_x]; \
-    slot_ch1_2 <= A11[cur_feature_x]; \
-    slot_ch1_3 <= A12[cur_feature_x]; \
-    slot_ch1_4 <= A13[cur_feature_x]; \
-    slot_ch1_5 <= A14[cur_feature_x]; \
-    slot_ch2_1 <= A20[cur_feature_x]; \
-    slot_ch2_2 <= A21[cur_feature_x]; \
-    slot_ch2_3 <= A22[cur_feature_x]; \
-    slot_ch2_4 <= A23[cur_feature_x]; \
-    slot_ch2_5 <= A24[cur_feature_x]; \
+`define CAPTURE_SLOT(A00,A01,A02,A03,A04,A10,A11,A12,A13,A14,A20,A21,A22,A23,A24,BANK,IDX) begin \
+    slot_ch0_1 <= A00[BANK][IDX]; \
+    slot_ch0_2 <= A01[BANK][IDX]; \
+    slot_ch0_3 <= A02[BANK][IDX]; \
+    slot_ch0_4 <= A03[BANK][IDX]; \
+    slot_ch0_5 <= A04[BANK][IDX]; \
+    slot_ch1_1 <= A10[BANK][IDX]; \
+    slot_ch1_2 <= A11[BANK][IDX]; \
+    slot_ch1_3 <= A12[BANK][IDX]; \
+    slot_ch1_4 <= A13[BANK][IDX]; \
+    slot_ch1_5 <= A14[BANK][IDX]; \
+    slot_ch2_1 <= A20[BANK][IDX]; \
+    slot_ch2_2 <= A21[BANK][IDX]; \
+    slot_ch2_3 <= A22[BANK][IDX]; \
+    slot_ch2_4 <= A23[BANK][IDX]; \
+    slot_ch2_5 <= A24[BANK][IDX]; \
+end
+
+`define CAPTURE_ACTIVE(BANK,IDX) begin \
+    case(active_slot) \
+        3'd0: `CAPTURE_SLOT(win0_ch0_0, win0_ch0_1, win0_ch0_2, win0_ch0_3, win0_ch0_4, win0_ch1_0, win0_ch1_1, win0_ch1_2, win0_ch1_3, win0_ch1_4, win0_ch2_0, win0_ch2_1, win0_ch2_2, win0_ch2_3, win0_ch2_4, BANK, IDX) \
+        3'd1: `CAPTURE_SLOT(win1_ch0_0, win1_ch0_1, win1_ch0_2, win1_ch0_3, win1_ch0_4, win1_ch1_0, win1_ch1_1, win1_ch1_2, win1_ch1_3, win1_ch1_4, win1_ch2_0, win1_ch2_1, win1_ch2_2, win1_ch2_3, win1_ch2_4, BANK, IDX) \
+        3'd2: `CAPTURE_SLOT(win2_ch0_0, win2_ch0_1, win2_ch0_2, win2_ch0_3, win2_ch0_4, win2_ch1_0, win2_ch1_1, win2_ch1_2, win2_ch1_3, win2_ch1_4, win2_ch2_0, win2_ch2_1, win2_ch2_2, win2_ch2_3, win2_ch2_4, BANK, IDX) \
+        3'd3: `CAPTURE_SLOT(win3_ch0_0, win3_ch0_1, win3_ch0_2, win3_ch0_3, win3_ch0_4, win3_ch1_0, win3_ch1_1, win3_ch1_2, win3_ch1_3, win3_ch1_4, win3_ch2_0, win3_ch2_1, win3_ch2_2, win3_ch2_3, win3_ch2_4, BANK, IDX) \
+        3'd4: `CAPTURE_SLOT(win4_ch0_0, win4_ch0_1, win4_ch0_2, win4_ch0_3, win4_ch0_4, win4_ch1_0, win4_ch1_1, win4_ch1_2, win4_ch1_3, win4_ch1_4, win4_ch2_0, win4_ch2_1, win4_ch2_2, win4_ch2_3, win4_ch2_4, BANK, IDX) \
+    endcase \
 end
 
 `define STORE_WIN(A0,A1,A2,A3,A4) begin \
     case(load_col_mod) \
         2'd0: begin \
-            if(load_fx < FEATURE_WIDTH) A0[load_fx] <= IMAGE_RAM_DIN; \
-            if(load_fx != 0) A3[load_fx - 1] <= IMAGE_RAM_DIN; \
+            if(load_fx < FEATURE_WIDTH) A0[load_bank][load_low] <= IMAGE_RAM_DIN; \
+            if(load_fx != 0) A3[load_prev_bank][load_prev_low] <= IMAGE_RAM_DIN; \
         end \
         2'd1: begin \
-            if(load_fx < FEATURE_WIDTH) A1[load_fx] <= IMAGE_RAM_DIN; \
-            if(load_fx != 0) A4[load_fx - 1] <= IMAGE_RAM_DIN; \
+            if(load_fx < FEATURE_WIDTH) A1[load_bank][load_low] <= IMAGE_RAM_DIN; \
+            if(load_fx != 0) A4[load_prev_bank][load_prev_low] <= IMAGE_RAM_DIN; \
         end \
         default: begin \
-            if(load_fx < FEATURE_WIDTH) A2[load_fx] <= IMAGE_RAM_DIN; \
+            if(load_fx < FEATURE_WIDTH) A2[load_bank][load_low] <= IMAGE_RAM_DIN; \
         end \
     endcase \
 end
@@ -77,14 +87,18 @@ module Convolver
     localparam WAIT_IMAGE    = 4'd5;
     localparam STORE_IMAGE   = 4'd6;
     localparam CLEAR_ACC     = 4'd7;
-    localparam READ_SLOT     = 4'd8;
-    localparam READ_OPERAND  = 4'd9;
-    localparam MUL_MAC       = 4'd10;
-    localparam ACCUMULATE    = 4'd11;
-    localparam WRITE_FEATURE = 4'd12;
-    localparam DONE          = 4'd13;
+    localparam READ_BANK0    = 5'd8;
+    localparam READ_BANK1    = 5'd9;
+    localparam READ_BANK2    = 5'd10;
+    localparam READ_BANK3    = 5'd11;
+    localparam READ_OPERAND  = 5'd12;
+    localparam MUL_MAC       = 5'd13;
+    localparam SUM_MAC       = 5'd14;
+    localparam ACCUMULATE    = 5'd15;
+    localparam WRITE_FEATURE = 5'd16;
+    localparam DONE          = 5'd17;
 
-    reg [3:0] cur_state, next_state;
+    reg [4:0] cur_state, next_state;
     reg [6:0] cur_filter_idx, next_filter_idx;
     reg [6:0] cur_load_row, next_load_row;
     reg [8:0] cur_load_idx, next_load_idx;
@@ -146,10 +160,20 @@ module Convolver
                            (cur_load_idx - 2*IMAGE_WIDTH));
     wire [5:0] load_fx = cur_load_fx;
     wire [1:0] load_col_mod = cur_load_col_mod;
+    wire [1:0] load_bank = cur_load_fx[4:3];
+    wire [2:0] load_low = cur_load_fx[2:0];
+    wire [5:0] load_prev_fx = cur_load_fx - 1;
+    wire [1:0] load_prev_bank = load_prev_fx[4:3];
+    wire [2:0] load_prev_low = load_prev_fx[2:0];
     wire [2:0] load_slot = cur_load_row % 5;
 
     wire [6:0] active_abs_row = cur_feature_y * 3 + cur_kernel_row;
     wire [2:0] active_slot = active_abs_row % 5;
+    wire [1:0] feature_bank = cur_feature_x[4:3];
+    wire [2:0] feature_low = cur_feature_x[2:0];
+    wire [4:0] read_state = (feature_bank == 0) ? READ_BANK0 :
+                            (feature_bank == 1) ? READ_BANK1 :
+                            (feature_bank == 2) ? READ_BANK2 : READ_BANK3;
 
     wire [6:0] filter_base = cur_channel * FILTER_WIDTH * FILTER_WIDTH +
                              cur_kernel_row * FILTER_WIDTH;
@@ -238,14 +262,17 @@ module Convolver
             cur_channel <= next_channel;
             cur_acc <= next_acc;
 
-            if(cur_state == READ_SLOT) begin
-                case(active_slot)
-                    3'd0: `CAPTURE_SLOT(win0_ch0_0, win0_ch0_1, win0_ch0_2, win0_ch0_3, win0_ch0_4, win0_ch1_0, win0_ch1_1, win0_ch1_2, win0_ch1_3, win0_ch1_4, win0_ch2_0, win0_ch2_1, win0_ch2_2, win0_ch2_3, win0_ch2_4)
-                    3'd1: `CAPTURE_SLOT(win1_ch0_0, win1_ch0_1, win1_ch0_2, win1_ch0_3, win1_ch0_4, win1_ch1_0, win1_ch1_1, win1_ch1_2, win1_ch1_3, win1_ch1_4, win1_ch2_0, win1_ch2_1, win1_ch2_2, win1_ch2_3, win1_ch2_4)
-                    3'd2: `CAPTURE_SLOT(win2_ch0_0, win2_ch0_1, win2_ch0_2, win2_ch0_3, win2_ch0_4, win2_ch1_0, win2_ch1_1, win2_ch1_2, win2_ch1_3, win2_ch1_4, win2_ch2_0, win2_ch2_1, win2_ch2_2, win2_ch2_3, win2_ch2_4)
-                    3'd3: `CAPTURE_SLOT(win3_ch0_0, win3_ch0_1, win3_ch0_2, win3_ch0_3, win3_ch0_4, win3_ch1_0, win3_ch1_1, win3_ch1_2, win3_ch1_3, win3_ch1_4, win3_ch2_0, win3_ch2_1, win3_ch2_2, win3_ch2_3, win3_ch2_4)
-                    3'd4: `CAPTURE_SLOT(win4_ch0_0, win4_ch0_1, win4_ch0_2, win4_ch0_3, win4_ch0_4, win4_ch1_0, win4_ch1_1, win4_ch1_2, win4_ch1_3, win4_ch1_4, win4_ch2_0, win4_ch2_1, win4_ch2_2, win4_ch2_3, win4_ch2_4)
-                endcase
+            if(cur_state == READ_BANK0) begin
+                `CAPTURE_ACTIVE(2'd0, feature_low)
+            end
+            else if(cur_state == READ_BANK1) begin
+                `CAPTURE_ACTIVE(2'd1, feature_low)
+            end
+            else if(cur_state == READ_BANK2) begin
+                `CAPTURE_ACTIVE(2'd2, feature_low)
+            end
+            else if(cur_state == READ_BANK3) begin
+                `CAPTURE_ACTIVE(2'd3, feature_low)
             end
             else if(cur_state == READ_OPERAND) begin
                 case(cur_channel)
@@ -393,15 +420,27 @@ module Convolver
                 next_acc = 0;
                 next_kernel_row = 0;
                 next_channel = 0;
-                next_state = READ_SLOT;
+                next_state = read_state;
             end
-            READ_SLOT: begin
+            READ_BANK0: begin
+                next_state = READ_OPERAND;
+            end
+            READ_BANK1: begin
+                next_state = READ_OPERAND;
+            end
+            READ_BANK2: begin
+                next_state = READ_OPERAND;
+            end
+            READ_BANK3: begin
                 next_state = READ_OPERAND;
             end
             READ_OPERAND: begin
                 next_state = MUL_MAC;
             end
             MUL_MAC: begin
+                next_state = SUM_MAC;
+            end
+            SUM_MAC: begin
                 next_state = ACCUMULATE;
             end
             ACCUMULATE: begin
@@ -414,7 +453,7 @@ module Convolver
                 else if(cur_kernel_row != 4) begin
                     next_channel = 0;
                     next_kernel_row = cur_kernel_row + 1;
-                    next_state = READ_SLOT;
+                    next_state = read_state;
                 end
                 else begin
                     next_channel = 0;
--- MAC.v
+++ MAC.v
@@ -26,8 +26,9 @@ reg signed [2*DATA_BW-1:0] mul_reg2;
 reg signed [2*DATA_BW-1:0] mul_reg3;
 reg signed [2*DATA_BW-1:0] mul_reg4;
 reg signed [2*DATA_BW-1:0] mul_reg5;
+reg signed [2*DATA_BW-1:0] sum_reg;
 
-assign MUL_DATA_OUT = mul_reg1 + mul_reg2 + mul_reg3 + mul_reg4 + mul_reg5;
+assign MUL_DATA_OUT = sum_reg;
 
 always @(posedge CLK or negedge RSTN) begin
     if(!RSTN) begin
@@ -36,8 +37,10 @@ always @(posedge CLK or negedge RSTN) begin
         mul_reg3 <= 0;
         mul_reg4 <= 0;
         mul_reg5 <= 0;
+        sum_reg <= 0;
     end
     else begin
+        sum_reg <= mul_reg1 + mul_reg2 + mul_reg3 + mul_reg4 + mul_reg5;
         if(MUL) begin
             mul_reg1 <= IFMAP_DATA_IN1 * FILTER_DATA_IN1;
             mul_reg2 <= IFMAP_DATA_IN2 * FILTER_DATA_IN2;
